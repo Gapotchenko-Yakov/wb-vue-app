8fcf19bc7f67b12f834941ed2dbe75abdbbeb2cf
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-17 21:15:30 +0300

    fix: fix proxy path replacement for vercel

--- api/proxy.ts
+++ api/proxy.ts
@@ -3,8 +3,7 @@ import fetch from 'node-fetch';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     // Получаем путь после /api/proxy
-    const path = req.url?.replace(/^\/api\/proxy/, '') || '';
-    // Собираем query-параметры
+    const path = req.url?.replace(/^\/api\/proxy/, '/api') || '';    // Собираем query-параметры
     const queryParams = new URLSearchParams({ ...req.query } as Record<string, string>).toString();
     // Запрос к реальному серверу
     const apiRes = await fetch(`http://109.73.206.144:6969${path}?${queryParams}`);
